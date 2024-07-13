aee236df0e26831506c1c4078c61cf7513dcf9e2
suryaanshchawla <suryaanshchawla@gmail.com>
2024-07-13 09:51:33 +0530

    seperated style and script

--- script.js
+++ script.js
@@ -1,58 +1,11 @@
-const commands = {};
-
-const term = $('body').terminal(commands);
-
-const greetings = `  ______                    _             __   ____             __  ____      ___     
- /_  __/__  _________ ___  (_)___  ____ _/ /  / __ \\____  _____/ /_/ __/___  / (_)___ 
-  / / / _ \\/ ___/ __ \`__ \\/ / __ \\/ __ \`/ /  / /_/ / __ \\/ ___/ __/ /_/ __ \\/ / / __ \\
- / / /  __/ /  / / / / / / / / / / /_/ / /  / ____/ /_/ / /  / /_/ __/ /_/ / / / /_/ /
-/_/  \\___/_/  /_/ /_/ /_/_/_/ /_/\\__,_/_/  /_/    \\____/_/   \\__/_/  \\____/_/_/\\____/`
-
-const term = $('body').terminal(commands, {
-    greetings
-});
-
-
-const font = 'Slant';
-
-figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/' });
-figlet.preloadFonts([font], ready);
-
-
-const term = $('body').terminal(commands, {
-    greetings: false
-});
-
-term.pause();
-
-function ready() {
-   term.echo(() => render('Terminal Portfolio')).resume();
+async function fetchNASAImageOfTheDay() {
+    try {
+        const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=PnJpYNWFNe9YNtwZW3VrrVXW30HBJa9en2sg9FtQ');
+        const data = await response.json();
+        const imageUrl = data.url;
+        document.body.style.backgroundImage = `url(${imageUrl})`;
+    } catch (error) {
+        console.error('Error fetching NASA image:', error);
+    }
 }
-
-function render(text) {
-    const cols = term.cols();
-    return trim(figlet.textSync(text, {
-        font: font,
-        width: cols,
-        whitespaceBreak: true
-    }));
-}
-
-function trim(str) {
-    return str.replace(/[\n\s]+$/, '');
-}
-
-
-function rainbow(string) {
-    return lolcat.rainbow(function(char, color) {
-        char = $.terminal.escape_brackets(char);
-        return `[[;${hex(color)};]${char}]`;
-    }, string).join('\n');
-}
-
-function hex(color) {
-    return '#' + [color.red, color.green, color.blue].map(n => {
-        return n.toString(16).padStart(2, '0');
-    }).join('');
-}
-
+fetchNASAImageOfTheDay();
